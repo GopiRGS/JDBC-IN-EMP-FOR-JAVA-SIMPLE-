a06aafff360312061e3efb7c57cda9ccf4c51bd6
GopiRGS <gopi272006@gmail.com>
2024-10-12 08:51:14 +0100

    Create COMMINT IN JDBC.java

---
+++ COMMINT IN JDBC.java
@@ -0,0 +1,71 @@
+
+
+
+
+import java.sql.*;
+
+public class JDBCDemo {
+    private static final String URL = "jdbc:mysql://localhost:3306/jdbcdemo1"; // Make sure this is the correct DB
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Velavan@2019";
+
+    public static void main(String[] args) {
+        try {
+            commitdemo();
+        } catch (Exception e) {
+            e.printStackTrace(); // Print any exceptions
+        }
+    }
+
+    public static void commitdemo() throws Exception{
+      String URL = "jdbc:mysql://localhost:3306/jdbcdemo1"; // Make sure this is the correct DB
+      String USERNAME = "root";
+      String PASSWORD = "Velavan@2019";
+      
+      String query1 = "update employee set salary = 400000 WHERE emp_id = 1";
+      String query2 = "update employee set salary = 400000 WHERE emp_id = 2";
+      
+      Connection con = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+      Statement st = con.createStatement();
+     int rows1=  st.executeUpdate(query1);
+     System.out.println("Number of rows affected : "+rows1);
+     
+     
+     int rows2 = st.executeUpdate(query2);
+     System.out.println("NUMber of affected : "+rows2);
+      
+    	
+    }
+   
+}
+
+
+
+
+
+//SQL CODE 
+
+
+create database jdbcdemo1;
+
+use jdbcdemo1;
+
+create table employee(
+emp_id int primary key,
+ename varchar(30),
+salary int
+);
+
+insert into employee values (1,"Ram",100000);
+
+select * from employee;
+
+insert into employee values(2,"Gopi",20000);
+
+insert into employee values(3,"Gokul",400000);
+
+
+
+
+
+
